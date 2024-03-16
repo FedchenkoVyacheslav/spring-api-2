01bc298c0f13f9c8598eacc6afb805a3e220a046
Viacheslav Fedchenko <viacheslav.fedchenko@piano.io>
2024-03-16 13:41:21 +0300

    add editUser page

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -1,10 +1,13 @@
 package org.example.controller;
 
+import org.example.domain.Identity;
+import org.example.domain.Role;
 import org.example.repository.IdentityRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +20,12 @@ public class UserController {
     public String userList(Model model){
         model.addAttribute("users", identityRepo.findAll());
         return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable Identity user, Model model){
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "editUser";
     }
 }
