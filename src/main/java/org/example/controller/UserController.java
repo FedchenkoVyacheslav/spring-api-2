c1625e96b71d03c2147702aa18cbf3458c8b0276
Viacheslav Fedchenko <viacheslav.fedchenko@piano.io>
2024-03-20 15:19:30 +0300

    add photo to profile

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -3,15 +3,24 @@ package org.example.controller;
 import org.example.domain.User;
 import org.example.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserRepo UserRepo;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("{user}")
     public String userEditForm(@PathVariable User user, Model model){
@@ -25,12 +34,27 @@ public class UserController {
             @RequestParam String surname,
             @RequestParam String location,
             @RequestParam int age,
+            @RequestParam("file") MultipartFile file,
             @RequestParam("userId") User user
-    ){
+    ) throws IOException {
         user.setName(name);
         user.setSurname(surname);
         user.setLocation(location);
         user.setAge(age);
+
+        if (!file.isEmpty() && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+            String uuidFileUUID = UUID.randomUUID().toString();
+            String resFileName = uuidFileUUID + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resFileName));
+
+            user.setPhotoUrl(resFileName);
+        }
+
         UserRepo.save(user);
 
         return "redirect:/main";
