64bf763f4391a8d01076f9ae509df74ec76baca3
FedchenkoVyacheslav <60151556+FedchenkoVyacheslav@users.noreply.github.com>
2024-03-22 01:56:58 +0400

    small fix

--- src/main/java/org/example/domain/User.java
+++ src/main/java/org/example/domain/User.java
@@ -16,7 +16,7 @@ import java.util.Set;
 @Table(name = "user")
 public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String email;
     private String password;
