1ca1959b42c49e79813bf29457061aaeb07ddda4
Viacheslav Fedchenko <viacheslav.fedchenko@piano.io>
2024-03-26 16:16:09 +0300

    add captcha functionality

--- src/main/java/org/example/controller/RegistrationController.java
+++ src/main/java/org/example/controller/RegistrationController.java
@@ -2,20 +2,32 @@ package org.example.controller;
 
 import jakarta.validation.Valid;
 import org.example.domain.User;
+import org.example.domain.dto.CaptchaResponseDto;
 import org.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
+    private final static String CAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
     @Autowired
     private UserService userService;
+
+    @Value("${recaptcha.secret}")
+    private String secret;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/registration")
     public String registration() {
@@ -24,9 +36,17 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(@Valid User user,
+                          @RequestParam("g-recaptcha-response") String captchaResponse,
                           BindingResult bindingResult,
                           Model model) {
-        if (bindingResult.hasErrors()) {
+        String url = String.format(CAPTCHA_URL, secret, captchaResponse);
+        CaptchaResponseDto response = restTemplate.postForObject(url, Collections.emptyList(), CaptchaResponseDto.class);
+
+        if (!response.isSuccess()) {
+            model.addAttribute("captchaError", "Fill captcha");
+        }
+
+        if (bindingResult.hasErrors() || !response.isSuccess()) {
             Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
             model.mergeAttributes(errors);
             return "registration";
