479812bcaa0bba1fbb626d100e84aedc45406fc3
FedchenkoVyacheslav <60151556+FedchenkoVyacheslav@users.noreply.github.com>
2024-03-31 17:07:45 +0400

    sort messages by date

--- src/main/java/org/example/repository/MessageRepo.java
+++ src/main/java/org/example/repository/MessageRepo.java
@@ -1,14 +1,11 @@
 package org.example.repository;
 
 import org.example.domain.Message;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public interface MessageRepo extends CrudRepository<Message, Integer> {
     List<Message> findByTitleContainingIgnoreCase(String title);
